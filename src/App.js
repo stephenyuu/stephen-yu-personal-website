8c666b2c083559bac955ffb9b81555a603df69a0
Stephen Yu <stephenyu1229@gmail.com>
2023-05-02 18:22:39 -0400

    refractor app to use nav bar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Nav from './components/nav';
 import Header from './components/header';
 import "./App.css";
 
 function App() {
   return (
     <div className="container">
+      <Nav />
       <Header />
     </div>
   );
