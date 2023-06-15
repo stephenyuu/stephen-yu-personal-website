669d0e206d12e789ea0b54b3cedf576d301c43eb
Stephen Yu <stephenyu1229@gmail.com>
2023-06-15 14:30:30 -0400

    start creating projects accordion

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,7 +1,48 @@
 import React from "react";
+import projectDescriptions from "./project-descriptions.json";
 
 const Projects = () => {
-    return <h1>put projects here</h1>
-}
+  const getImagePath = (imageName) => {
+    return require(`../../assets/photos/${imageName}`).default;
+  }
+  return (
+    <div
+      id="projects"
+      className="d-flex justify-content-center align-items-center my-section my-content"
+    >
+      <div className="my-accordion">
+        {projectDescriptions.map((proj, index) => (
+          <div className="my-accordion-panel">
+            <h4 id={`panel${index}-heading`}>
+              <button
+                className="my-accordion-trigger"
+                aria-controls={`panel${index}-content`}
+                aria-expanded="true"
+              >
+                <span id={`panel${index}-title`}>{proj.name}</span>
+              </button>
+            </h4>
+            <div
+              className="my-accordion-content"
+              id={`panel${index}-content`}
+              aria-labelledby="panel1-heading"
+              aria-hidden="false"
+              role="region"
+            >
+              <p>
+                {proj.description}
+              </p>
+              <img
+                className="my-accordion-image"
+                src={getImagePath(proj.image)}
+                alt="Chomp Logo"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
