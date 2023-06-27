0d11f957352f606a0bde77d27a881352ca2e1bfc
Stephen Yu <stephenyu1229@gmail.com>
2023-06-27 13:46:08 -0400

    finish building projects accordion, need to add more project info

--- src/components/projects/projects-accordion/index.js
+++ src/components/projects/projects-accordion/index.js
@@ -10,44 +10,52 @@ const ProjectsAccordion = () => {
   };
 
   return (
-    <div className="mt-3 my-project-accordion">
-      {projectDescriptions.map((proj, index) => {
-        const isActivePanel = index === activeIndex;
-        const projectSvg = proj.image.substring(0, proj.image.indexOf("."));
-        return (
-          <div
-            className="my-project-panel"
-            onClick={() => setActiveIndex(index)}
-          >
-            <h4 id={`panel${index}-heading`}>
-              <button
-                className="my-project-accordion-trigger"
-                aria-controls={`panel${index}-content`}
-                aria-expanded={isActivePanel ? "true" : "false"}
-              >
-                <span className="my-project-accordion-title" id={`panel${index}-title`}>{proj.name}</span>
-                <svg aria-hidden="true" className="my-project-accordion-icon">
-                  <use href={`${projectIcons}#${projectSvg}`}></use>
-                </svg>
-              </button>
-            </h4>
+    <div className="mt-3 my-project-accordion-wrapper">
+      <div className="my-project-accordion">
+        {projectDescriptions.map((proj, index) => {
+          const isActivePanel = index === activeIndex;
+          const projectSvg = proj.image.substring(0, proj.image.indexOf("."));
+          return (
             <div
-              className="my-project-content"
-              id={`panel${index}-content`}
-              aria-labelledby={`panel${index}-heading`}
-              aria-hidden={isActivePanel ? "false" : "true"}
-              role="region"
+              className="my-project-panel"
+              onClick={() => setActiveIndex(index)}
             >
-              <p>{proj.description}</p>
-              <img
-                className="my-project-accordion-image"
-                src={getImagePath(proj.image)}
-                alt={proj.name}
-              />
+              <h4 id={`panel${index}-heading`}>
+                <button
+                  className="my-project-accordion-trigger"
+                  aria-controls={`panel${index}-content`}
+                  aria-expanded={isActivePanel ? "true" : "false"}
+                >
+                  <span
+                    className="my-project-accordion-title"
+                    id={`panel${index}-title`}
+                  >
+                    {proj.name}
+                  </span>
+                  <svg aria-hidden="true" className="my-project-accordion-icon">
+                    <use href={`${projectIcons}#${projectSvg}`}></use>
+                  </svg>
+                </button>
+              </h4>
+              <div
+                className="my-project-content"
+                id={`panel${index}-content`}
+                aria-labelledby={`panel${index}-heading`}
+                aria-hidden={isActivePanel ? "false" : "true"}
+                role="region"
+              >
+                <p className="mt-2">{proj.description}</p>
+                
+                <img
+                  className="my-project-accordion-image"
+                  src={getImagePath(proj.image)}
+                  alt={proj.name}
+                />
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
