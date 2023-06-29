ccf770c6daca21277f015c533545bb77f7d50bb3
Stephen Yu <stephenyu1229@gmail.com>
2023-06-29 18:07:50 -0400

    add logo to nav bar

--- src/components/education/index.js
+++ src/components/education/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Education = () => (
   <div
     id="education"
-    className="d-flex flex-column"
+    className="d-flex flex-column justify-content-center align-items-center my-section my-content"
   >
     <h1>Hello put education here</h1>
   </div>
--- src/components/header-nav/index.js
+++ src/components/header-nav/index.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
 import navLinks from "./nav-links.json";
+import projectIcons from "../../assets/svgs/icons.svg";
 import "./index.css";
 
 const HeaderNav = () => {
   return (
     <Navbar className="mt-3 justify-content-between" expand="md">
       <Navbar.Brand className="my-logo">
-        
+        <a href="/">
+          <svg aria-hidden="true" className="my-logo-icon">
+            <use href={`${projectIcons}#logo`}></use>
+          </svg>
+        </a>
       </Navbar.Brand>
       <Navbar.Toggle className="my-navbar-button" />
       <Navbar.Collapse className="my-navbar-collapse">
         <Nav>
           {navLinks.map((link) => (
-            <Nav.Link key={link.name} href={link.url}>{link.name}</Nav.Link>
+            <Nav.Link key={link.name} href={link.url}>
+              {link.name}
+            </Nav.Link>
           ))}
         </Nav>
       </Navbar.Collapse>
