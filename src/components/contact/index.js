adb8ebc26dbd517d884b4db825fd0eb3530d1c63
Stephen Yu <stephenyu1229@gmail.com>
2023-06-30 03:13:55 -0400

    add contact me section

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,12 +1,19 @@
 import React from "react";
+import "./index.css";
 
 const Contact = () => {
   return (
     <div
       id="contact"
-      className="d-flex justify-content-center align-items-center my-section my-content"
+      className="d-flex flex-column justify-content-center align-items-center my-section my-content"
     >
-      <h2>Contact</h2>
+      <h2>Get In Touch</h2>
+      <p className="mt-2">
+        Shoot me an email if you want to share anything with me, whether it be
+        project ideas, questions, or just to say what's up. I'll try my best to
+        get back to you!
+      </p>
+      <a className="mt-5 my-email-button" href="mailto:stephenyu1229@gmail.com">Say What's Up</a>
     </div>
   );
 };
